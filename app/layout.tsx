786f6cec69adca21af24d2ac3b67a811ae20db97
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2026-02-25 17:48:51 -0500

    Add Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "JumboCode Team Compare",
@@ -34,6 +35,7 @@ export default function RootLayout({
           <main>{children}</main>
         </div>
        </body>
+       <Analytics />
     </html>
   );
 }
