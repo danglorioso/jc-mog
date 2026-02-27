f4dab5492f7505927d983579cb88cc1f9cd7f7b5
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2026-02-27 00:34:44 -0500

    Feat: add private JC repos

--- lib/repos.ts
+++ lib/repos.ts
@@ -4,4 +4,11 @@ export const REPOS = [
   { repo: "jumbocode/boston-community-pediatrics", name: "Boston Community Pediatrics" },
   { repo: "jumbocode/bostons-higher-ground", name: "Boston's Higher Ground" },
   { repo: "jumbocode/food-for-free", name: "Food for Free" },
+  { repo: "jumbocode/lexington-zero-waste", name: "Lexington Zero Waste" },
+  { repo: "jumbocode/school-on-wheels", name: "School on Wheels" },
+  { repo: "jumbocode/the-loop-lab", name: "The Loop Lab" },
+  { repo: "jumbocode/artists-for-humanity", name: "Artists for Humanity" },
+  { repo: "jumbocode/commonwealth-kitchen", name: "Commonwealth Kitchen" },
+  { repo: "jumbocode/breaktime", name: "Breaktime" },
+  { repo: "jumbocode/food-link", name: "Food Link" },
 ];
